17a62cccbeded838c8498af6eae1982a30e2320a
Jared Hanson <jaredhanson@gmail.com>
2023-08-09 05:28:41 -0700

    DSL for setting username and password envvars.

--- credentials.sh
+++ credentials.sh
@@ -1,3 +1,13 @@
+# use password google
+# use password google/foo FOO
+# TODO: add support for passing envvar prefix
+use_password() {
+  echo "Password $1"
+  local provider="pass"
+  eval "${provider}_password" "$@"
+}
+
+
 credential () {
   echo "todo"
 }
@@ -16,14 +26,32 @@ password () {
 
 
 pass_password () {
-  echo "pass_password ---" >&2
+  local prefix=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+  #echo "prefix is $prefix" >&2
   
   local out=$(pass show $1)
-  echo "password: $out" >&2
-  echo "$out"
+  
+  IFS=$'\n' read -d '' -r -a lines <<< "$out"
+  
+  # TODO: Exaport first line as password, only parse subsequent lines for metadata
+  export "${prefix}_PASSWORD=todo"
+  
+  while IFS=: read -ra arr; do
+    echo "arr is $arr[@]" >&2
+    echo "arr0 is ${arr[0]}" >&2
+    echo "arr1 is ${arr[1]}" >&2
+    
+    # TODO: set url/location/etc
+    # TODO: case insensitive compare
+    if [ ${arr[0]} == "Username" ]; then
+      echo "found username" >&2
+      # TODO: trim whitepace, maybe in IFC read above?
+      export "${prefix}_USERNAME=${arr[1]}"
+    fi
+  done <<< "$out"
 }
 
-pass_username () {
+x_pass_username () {
   local out=$(pass show $1)
   
   #IFS= read -ra LINES <<< "$out"
