a3371ffa6a5240859df05715f5f1f61f58feba88
Jared Hanson <jaredhanson@gmail.com>
2023-08-17 06:08:13 -0700

    Clean up.

--- credentials/pass.sh
+++ credentials/pass.sh
@@ -6,24 +6,26 @@ pass_password () {
   local name=$(echo "$1" | tr '[:lower:]' '[:upper:]')
   
   local out=$(pass show $1)
-  echo "out is $out" >&2
+  echo "==" >&2
+  echo "$out" >&2
+  echo "==" >&2
   
   #IFS=$'\n' read -d '' -r -a lines <<< "$out"
   
   # TODO: Exaport first line as password, only parse subsequent lines for metadata
   export "${name}_PASSWORD=todo"
   
-  while IFS=: read -ra arr; do
-    echo "arr is $arr[@]" >&2
-    echo "arr0 is ${arr[0]}" >&2
-    echo "arr1 is ${arr[1]}" >&2
+  while IFS=: read -ra fields; do
+    echo "fields is ${fields[@]}" >&2
+    echo "fields0 is ${fields[0]}" >&2
+    echo "fields1 is ${fields[1]}" >&2
     
     # TODO: set url/location/etc
     # TODO: case insensitive compare
-    if [ ${arr[0]} == "Username" ]; then
+    if [ ${fields[0]} == "Username" ]; then
       echo "found username" >&2
       # TODO: trim whitepace, maybe in IFC read above?
-      export "${name}_USERNAME=${arr[1]}"
+      export "${name}_USERNAME=${fields[1]}"
     fi
   done <<< "$out"
 }
