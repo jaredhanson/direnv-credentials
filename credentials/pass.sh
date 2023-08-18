9370c9f5069e8ded5a6bae3e3d77b069c5ea78ab
Jared Hanson <jaredhanson@gmail.com>
2023-08-18 05:48:04 -0700

    Clarify comments.

--- credentials/pass.sh
+++ credentials/pass.sh
@@ -3,6 +3,8 @@
 # Decrypts a password using `pass` and sets <NAME>_USERNAME and <NAME>_PASSWORD
 # environment variables.
 pass_password () {
+  # Uppercase the name of the password, which will be prefixed to the
+  # environment variables being set.
   local name=$(echo "$1" | tr '[:lower:]' '[:upper:]')
   
   local out=$(pass show $1)
@@ -11,13 +13,15 @@ pass_password () {
   echo "==" >&2
   
   {
+    # Read the first line of the output as the password.
     read -r password
     export "${name}_PASSWORD=${password}"
     
-    # Split each line of the output into the field and value using colon
-    # separator.  Note that the value itself may contain a colon, which should not
-    # split the value.  Due to that, the `-a` argument is not used to read into
-    # an array.
+    # Split each line of the output into a field and value using a colon
+    # separator.  Note that the value itself may contain a colon, which should
+    # not be split.  Due to that, the `-a` argument is not used to read into an
+    # array.  Rather, all characters after the first colon are assigned to
+    # value.
     while IFS=: read -r field value; do
       echo "- ${field}: ${value}" >&2
     
